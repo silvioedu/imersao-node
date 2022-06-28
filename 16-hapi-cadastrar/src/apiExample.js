1e3fec1cede6766285fbdb7ce89ccba78ad20b97
Silvio Silva <silvioedu@gmail.com>
2022-06-28 13:49:15 -0300

    estudo

--- 16-hapi-cadastrar/src/apiExample.js
+++ 16-hapi-cadastrar/src/apiExample.js
@@ -1,30 +1,28 @@
-const Hapi = require('@hapi/hapi')
-
+const Hapi = require('hapi')
 const Context = require('./db/strategies/base/contextStrategy')
 const MongoDb = require('./db/strategies/mongodb/mongodb')
 const HeroisSchema = require('./db/strategies/mongodb/schemas/heroisSchema')
-const HeroisRoutes = require('./routes/heroisRoutes')
 
 const app = new Hapi.Server({
     port: 5000
 })
-
-function mapRoutes(instance, methods) {
-    return methods.map(method => instance[method]())
-}
 
 async function main() {
     const connection = MongoDb.connect()
     const context = new Context(new MongoDb(connection, HeroisSchema))
 
     app.route([
-        ...mapRoutes(new HeroisRoutes(context), HeroisRoutes.methods())
+        {
+            path: '/herois',
+            method: 'GET',
+            handler: (request, head) => {
+                return context.read()
+            }
+        }
     ])
 
     await app.start()
     console.log('Servidor rodando na porta', app.info.port)
-
-    return app
 }
 
-module.exports = main()
+main()
